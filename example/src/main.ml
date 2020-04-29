2d2ce88f3e293072fabbfbcd2e633808843f6e58
Ding Xiang Fei <6884440+dingxiangfei2009@users.noreply.github.com>
2020-04-30 01:11:24 +0800

    handle panic in rust ffi functions to prevent unwinding into ocaml (#5)

--- example/src/main.ml
+++ example/src/main.ml
@@ -1,3 +1,5 @@
+exception TestFail
+
 let _ =
   let open Stubs in
   let v1 = { x = 1.; y= 2.; z = 3. } in
@@ -6,4 +8,11 @@ let _ =
   let v4 = add_vecs v1 v3 in
   let v5 = add_vecs v4 v2 in
   let sum = sum_vecs (Many [| v1; v2; v3; v4; v5 |]) { bound = 3 } in
-  Printf.printf "[ %.2f; %.2f; %.2f ]\n" sum.x sum.y sum.z
+  Printf.printf "[ %.2f; %.2f; %.2f ]\n" sum.x sum.y sum.z;
+
+  let open Stubs in
+  try
+    test_panic ();
+    raise TestFail
+  with
+  | RustFFI err -> Printf.printf "got rust ffi panic message: %s\n" err
